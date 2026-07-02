392e2494aff53169c162582acadcb3a4c7801c25
Dunya4 <dunyaliyeva9@gmail.com>
2026-07-02 22:08:59 +0400

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+const success = document.querySelector(".success");
+const error = document.querySelectorAll(".error");
+
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const general = document.getElementById("general");
+const support = document.getElementById("support");
+const message = document.getElementById("message");
+const confirm = document.getElementById("confirm");
+const button = document.getElementById("card_button");
+
+form.addEventListener("submit", function () {
+
+
+
+    
+})
+
